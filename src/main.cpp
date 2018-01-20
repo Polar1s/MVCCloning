011ae3d7cd3824afb0dfe478ee8fe047b2540d89
Polar1s <lbc1245@gmail.com>
2018-01-21 00:00:17 +0800

    Added timer

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/opencv.hpp>
 #include <cstdio>
 #include <string>
+#include <sys/time.h>
+#include <unistd.h>
 
 using namespace std;
 using namespace cv;
@@ -18,11 +20,19 @@ int main(int argc, char **argv) {
     int offsetX = argc >= 6 ? atoi(argv[5]) : 0;
     stitch.offset = Point(offsetX, offsetY);
 
+    // Time counter
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
     stitch.init(string(argv[1]));
     printf("init OK.\n");
 
     stitch.stitch(string(argv[2]), string(argv[3]));
     printf("Stitching finished.\n");
+
+    gettimeofday(&end, NULL);
+    double sec = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec) * 1e-6;
+    printf("Time elapsed: %.3lfs\n", sec);
 
     return 0;
 }
